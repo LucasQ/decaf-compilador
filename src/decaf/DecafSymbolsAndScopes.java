51b27a22de4093fed65e2eec83dfae099fe1fe08
LucasQ <mallevo20@gmail.com>
2018-11-01 08:35:15 -0300

    01/11

--- src/decaf/DecafSymbolsAndScopes.java
+++ src/decaf/DecafSymbolsAndScopes.java
@@ -1,6 +1,4 @@
 package decaf;
-
-import java.lang.System;
 import org.antlr.symtab.FunctionSymbol;
 import org.antlr.symtab.GlobalScope;
 import org.antlr.symtab.LocalScope;
@@ -11,7 +9,7 @@ import org.antlr.v4.runtime.misc.NotNull;
 import org.antlr.v4.runtime.ParserRuleContext;
 import org.antlr.v4.runtime.Token;
 import org.antlr.v4.runtime.tree.ParseTreeProperty;
-import java.util.*;
+
 
 /**
  * This class defines basic symbols and scopes for Decaf language
@@ -20,9 +18,7 @@ public class DecafSymbolsAndScopes extends DecafParserBaseListener {
     ParseTreeProperty<Scope> scopes = new ParseTreeProperty<Scope>();
     GlobalScope globals;
     Scope currentScope; // define symbols in this scope
-    static final int SAIDA = 0;
-    static final String NULL = "";
-    
+
     @Override
     public void enterProgram(DecafParser.ProgramContext ctx) {
         globals = new GlobalScope(null);
@@ -35,118 +31,78 @@ public class DecafSymbolsAndScopes extends DecafParserBaseListener {
     }
 
     @Override
-    public void enterField_decl(DecafParser.Field_declContext ctx) {
-
-        
-        int arraysize = Integer.parseInt(ctx.integer().get(0).NUMBER().getText());
-                        
-                if (arraysize <= SAIDA) {
-                    
-                    this.error(ctx.integer().get(0).NUMBER().getSymbol(), " tamanho de array não válido");
-                    System.exit(SAIDA);
-                }
-            
-            determinationVar(ctx.ID().get(SAIDA).getSymbol()); 
-    }
-
-    @Override
     public void enterMethod_decl(DecafParser.Method_declContext ctx) {
-        String name = ctx.ID().get(SAIDA).getText();
-        int typeTokenType = ctx.type().start.getType();
-        DecafSymbol.Type type = this.getType(typeTokenType);
+        String name = ctx.ID().get(0).getText();
+        //int typeTokenType = ctx.type().start.getType();
+        //DecafSymbol.Type type = this.getType(typeTokenType);
 
         // push new scope by making new one that points to enclosing scope
         FunctionSymbol function = new FunctionSymbol(name);
-        //function.setType(type); // Set symbol type
+        // function.setType(type); // Set symbol type
 
         currentScope.define(function); // Define function in current scope
-        saveScope(function);
+        saveScope(ctx, function);
         pushScope(function);
     }
 
-    // @Override
-    // public void exitMethod_decl(DecafParser.Method_declContext ctx) {
-    //     popScope();
-    // }
+    DecafParser.Var_declContext varr;
+    @Override
+    public void enterVar_decl(DecafParser.Var_declContext ctx) { 
+        varr = ctx;
+    }
 
+    @Override 
+    public void exitVar_decl(DecafParser.Var_declContext ctx) { 
+        if(ctx.ID(0).getSymbol()==varr){
+            error(ctx.ID(0).getSymbol(),"deu ruim");
+            System.exit(0);
+        }
+    }
+
+    /*@Override
+    public void exitMethod_decl(DecafParser.Method_declContext ctx) {
+        popScope();
+    }
     @Override
     public void enterBlock(DecafParser.BlockContext ctx) {
-        // LocalScope l = new LocalScope(currentScope);
-        // saveScope(ctx, currentScope);
+        LocalScope l = new LocalScope(currentScope);
+        saveScope(ctx, currentScope);
         // pushScope(l);
     }
-
     @Override
     public void exitBlock(DecafParser.BlockContext ctx) {
-        //popScope();
+        popScope();
     }
-
     @Override
-    public void enterStatement(DecafParser.StatementContext stmt) {
-        List<Symbol> symbols = currentScope.getSymbols();
-        
-        if (!symbols.contains(new VariableSymbol(stmt.location().ID().getText()))) {
-            this.error(stmt.location().ID().getSymbol(), "wrong declaration identifier");
-            System.exit(SAIDA);
-        }else{
-            System.exit(1);
+    public void enterDecl(DecafParser.Field_declContext ctx) {
+        defineVar(ctx.type(), ctx.ID().getSymbol());
+    }
+    @Override
+    public void exitDecl(DecafParser.Field_declContext ctx) {
+        String name = ctx.ID().getSymbol().getText();
+        Symbol var = currentScope.resolve(name);
+        if ( var==null ) {
+            this.error(ctx.ID().getSymbol(), "no such variable: "+name);
+        }
+        if ( var instanceof FunctionSymbol ) {
+            this.error(ctx.ID().getSymbol(), name+" is not a variable");
         }
     }
-    
-    @Override
-    public void exitStatement(DecafParser.StatementContext ctx) {
-    }
-
-
-    @Override
-    public void enterVar_decl(DecafParser.Var_declContext ctx) {
-        Token name = ctx.ID().get(SAIDA).getSymbol();
-        this.determinationVar(name);
-    }
-
-
-    @Override
-    public void exitVar_decl(DecafParser.Var_declContext ctx) {
-    }
-
-
-    //     @Override
-    //     public void enterDecl(DecafParser.Field_declContext ctx) {
-    //         defineVar(ctx.type(), ctx.ID().getSymbol());
-    //     }
-
-    //     @Override
-    // public void exitDecl(DecafParser.Field_declContext ctx) {
-    //     String name = ctx.ID().getSymbol().getText();
-    //     Symbol var = currentScope.resolve(name);
-    //     if ( var==null ) {
-    //         this.error(ctx.ID().getSymbol(), "no such variable: "+name);
-    //     }
-    //     if ( var instanceof FunctionSymbol ) {
-    //         this.error(ctx.ID().getSymbol(), name+" is not a variable");
-    //     }
-    // }
-
-    void determinationVar(Token nameToken) {
-        //int typeTokenType = typeCtx.start.getType();
-        String var = new VariableSymbol(nameToken.getText());
-
+    void defineVar(DecafParser.TypeContext typeCtx, Token nameToken) {
+        int typeTokenType = typeCtx.start.getType();
+        VariableSymbol var = new VariableSymbol(nameToken.getText());
         // DecafSymbol.Type type = this.getType(typeTokenType);
         // var.setType(type);
-
         currentScope.define(var); // Define symbol in current scope
-        {
-        System.out.println("Var_decl: " + var.getName());
-        System.out.println("Var_esc: " + currentScope.getSymbolNames());
-        }
     }
-
+*/
     /**
      * Método que atuliza o escopo para o atual e imprime o valor
      *
      * @param s
      */
-    private void pushScope(Symbol s) {
+
+    private void pushScope(Scope s) {
         currentScope = s;
         System.out.println("entering: "+currentScope.getName()+":"+s);
     }
@@ -180,14 +136,13 @@ public class DecafSymbolsAndScopes extends DecafParserBaseListener {
      * @param tokenType
      * @return
      */
-    //// public static DecafSymbol.Type getType(int tokenType) {
-    //     switch ( tokenType ) {
-    //         case DecafParser.VOID :  return DecafSymbol.Type.tVOID;
-    //         case DecafParser.NUMBER :   return DecafSymbol.Type.tINT;
-    //     }
-    //     return DecafSymbol.Type.tINVALID;
-    // } java -jar dist/Compiler.jar -target inter ../semantics/legal-01.dcf
-   
+    public static DecafSymbol.Type getType(int tokenType) {
+        switch ( tokenType ) {
+            case DecafParser.VOID :  return DecafSymbol.Type.tVOID;
+            case DecafParser.NUMBER :   return DecafSymbol.Type.tINT;
+        }
+        return DecafSymbol.Type.tINVALID;
+    }
 
 
 }
--- src/decaf/Main.java
+++ src/decaf/Main.java
@@ -111,14 +111,18 @@ class Main {
 					//show AST in GUI
 					JFrame frame = new JFrame("Antlr AST");
 					JPanel panel = new JPanel();
+					JScrollPane scrollPane = new JScrollPane(panel);
+					scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+					scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_NEVER);
+					scrollPane.setBounds(50, 30, 300, 50);
 					TreeViewer viewr = new TreeViewer(Arrays.asList(
-							parser.getRuleNames()),tree);
+					parser.getRuleNames()),tree);
 					viewr.setScale(1.5);//scale a little
 					panel.add(viewr);
-					frame.add(panel);
+					frame.add(scrollPane);
 					frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-					frame.setSize(600,400);
-					frame.setVisible(true);
+					frame.setSize(1024,768);
+		  			frame.setVisible(true);
 				}
 
 
@@ -147,16 +151,19 @@ class Main {
 
 					//show AST in GUI
 					JFrame frame = new JFrame("Antlr AST");
-					JPanel panel = new JPanel();
-					JScrollPane scroll = new JScrollPane();
-					TreeViewer viewr = new TreeViewer(Arrays.asList(
-							parser.getRuleNames()),tree);
-					viewr.setScale(1.5);
-					panel.add(viewr);
-					frame.add(panel);
-					frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-					frame.setSize(600,400);
-					frame.setVisible(true);
+          JPanel panel = new JPanel();
+          JScrollPane scrollPane = new JScrollPane(panel);
+          scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+          scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_NEVER);
+          scrollPane.setBounds(50, 30, 300, 50);
+          TreeViewer viewr = new TreeViewer(Arrays.asList(
+          parser.getRuleNames()),tree);
+          viewr.setScale(1.5);//scale a little
+          panel.add(viewr);
+          frame.add(scrollPane);
+          frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+          frame.setSize(1024,768);
+		  frame.setVisible(true);
 				}
 
 			}
